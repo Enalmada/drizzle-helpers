17b8b37608d4641c7b277556696d41c51e82c366
Adam Lane <enalmada@gmail.com>
2023-09-21 18:16:01 -0700

    initial module (#1)

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { connectToDatabase } from './DrizzleConnect';
+import { createRepo, OrderBy, Page, Paging } from './DrizzleOrm';
+
+export { createRepo, connectToDatabase, OrderBy, Paging, Page };
