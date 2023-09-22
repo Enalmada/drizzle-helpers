4bed5164d503d0cd1446b4842fdb47ef60d6c747
Adam Lane <enalmada@gmail.com>
2023-09-21 17:28:33 -0700

    initial

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { connectToDatabase } from './DrizzleConnect';
+import { createRepo, OrderBy, Page, Paging } from './DrizzleOrm';
+export { createRepo, connectToDatabase, OrderBy, Paging, Page };
