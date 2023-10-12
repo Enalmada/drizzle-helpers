de14d1ffc51e30be8ed0d9c48bb16a722c6d15da
Adam Lane <enalmada@gmail.com>
2023-10-12 16:47:00 -0700

    automated externals in build

---
+++ build.ts
@@ -0,0 +1,33 @@
+import packageJson from './package.json';
+
+function getExternalsFromPackageJson(): string[] {
+  const sections: (keyof typeof packageJson)[] = [
+    'dependencies',
+    'devDependencies',
+    'peerDependencies',
+  ];
+  const externals: string[] = [];
+
+  for (const section of sections) {
+    if (packageJson[section]) {
+      externals.push(...Object.keys(packageJson[section]));
+    }
+  }
+
+  // Removing potential duplicates between dev and peer
+  return Array.from(new Set(externals));
+}
+
+async function buildWithExternals(): Promise<void> {
+  const externalDeps = getExternalsFromPackageJson();
+
+  await Bun.build({
+    entrypoints: ['./src/index.ts'],
+    outdir: './dist',
+    target: 'node',
+    external: externalDeps,
+    root: './src',
+  });
+}
+
+buildWithExternals();
