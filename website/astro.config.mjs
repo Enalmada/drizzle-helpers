56046b9575ce243050c7536083522c62f8fdb629
Adam Lane <enalmada@gmail.com>
2023-12-20 21:54:09 -0800

    dependency updates

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -51,6 +51,7 @@ export default defineConfig({
           ],
         },
       ],
+      customCss: ['./src/assets/landing.css', './src/tailwind.css'],
     }),
     react(),
     // applyBaseStyles causes lists to not work anymore
